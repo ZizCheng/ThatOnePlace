0b10c7d9556571902fa1a18857f1e491fdfb7434
Zizheng Cheng <zizhengcheng@zizhengs-mbp.lan>
2018-05-24 20:13:52 -0700

    some shit in restaurantVC

--- That One Place/RestaurantVC.swift
+++ That One Place/RestaurantVC.swift
@@ -8,17 +8,34 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class RestaurantVC: UIViewController
 {
     var restaurant: Restaurant?
     
+    @IBOutlet weak var restaurantImage: UIImageView!
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var restaurantPriceLabel: UILabel!
+    @IBOutlet weak var restaurantMapView: MKMapView!
     @objc override func viewDidLoad()
     {
         super.viewDidLoad()
         var swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
         swipeDown.direction = UISwipeGestureRecognizerDirection.down
         self.view.addGestureRecognizer(swipeDown)
+        
+        //restaurantImage.image = restaurant.image
+        
+        restaurantNameLabel.text = restaurant?.name
+        restaurantPriceLabel.text = restaurant?.price
+        
+        setMap()
+    }
+    
+    func setMap()
+    {
+        
     }
     
     @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
